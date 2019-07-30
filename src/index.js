348d98bbb45c99a2004684e249414f0d9641357a
Saiwarun Yenjitpissamai <saiwarun.1253@gmail.com>
2019-07-30 18:37:41 +0700

    setup babel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 const app = express()
 const port = process.env.PORT || 4000
-app.get('/system/health', (req, res) => res.sendStatus(200))
+app.get('/system/health', (req, res) => {
+    res.send('Hello world')
+})
 app.post('/webhook', (req, res) => res.sendStatus(200))
 console.log('running on port ', port)
 app.listen(port)
