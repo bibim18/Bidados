e4fb516d7bb5a92edda5cddbc4aef94bb9ff409d
Saiwarun Yenjitpissamai <saiwarun.1253@gmail.com>
2019-08-11 22:47:05 +0700

    test

--- src/index.js
+++ src/index.js
@@ -26,10 +26,11 @@ router.get('/test', async (ctx,next)=> {
 
 router.post('/webhook', async (ctx, next) => {
     let reply_token = ctx.request.body.events[0].replyToken
+    let resp = {}
     const postback = ctx.request.bodu.events[0].postback
     console.log('postback\n', postback)
     const message = ctx.request.body.events[0].message
-    if(message) const resp = await reply(reply_token, message)
+    if(message) resp = await reply(reply_token, message)
   ctx.body = resp
 })
 
